c85336b79e1ff45a5f92e77674ec3563d822c830
sota <git@xsota.com>
2015-12-19 13:07:08 +0900

    かぎを外に置いといて好きなときに中に呼びたいねん

--- bot.rb
+++ bot.rb
@@ -3,17 +3,43 @@
 
 require "twitter"
 
+class BOT 
+  API_KEY = ""
+  API_SECRET = ""
+  TOKEN = ""
+  TOKEN_SECRET = ""
+  
+  def initialize()
+    API_KEY << checkKeys("KEYS/API_KEY")
+    API_SECRET << checkKeys("KEYS/API_SECRET")
+    TOKEN << checkKeys("KEYS/TOKEN")
+    TOKEN_SECRET << checkKeys("KEYS/TOKEN_SECRET")
+  end
+  
+  def checkKeys(path)
+    #ファイルがあるけど中身が空の時もファイルが無かった時と同じようなメッセージ出すか
+    if File.exist?(path)
+      return File.read(path).chomp
+    end
+    
+    #ファイルが無かった時に出力する文章だれか考えて
+    File.open(path, "w").close()    
+    puts "ファイル無いよ"
+    return "unko"
+  end
 
-API_KEY = ""
-API_SECRET = ""
-TOKEN=""
-TOKEN_SECRET=""
+  def hoge()
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = API_KEY
+      config.consumer_secret = API_SECRET
+      config.access_token        = TOKEN
+      config.access_token_secret = TOKEN_SECRET
+    end
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key = API_KEY
-  config.consumer_secret = API_SECRET
-  config.access_token        = TOKEN
-  config.access_token_secret = TOKEN_SECRET
+    client.update("hogehoehogehoge")
+  end
+
 end
 
-client.update("Hello 清潔なトイレ")
+bot = BOT.new
+bot.hoge
